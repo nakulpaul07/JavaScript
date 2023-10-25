09d1f8d4e2ea22b25f1a643956e35c9b6fbcea8a
nakulpaul07 <nakulpal878@gmail.com>
2023-10-25 20:03:05 +0530

    new

---
+++ set.js
@@ -0,0 +1,4 @@
+document.getElementById("a").innerHTML = "<center><h2>Vivek Yadav Gwalior 123<h2><center>";
+document.getElementById("a").setAttribute("style","border:10px dotted black");
+// document.getElementById("a").removeAttribute("style");
+document.getElementById("b").innerHTML= "<h2>GWalior<h2>"
